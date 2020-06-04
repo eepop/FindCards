325cf50fad056b66bbf99160913bc5d74f9e3634
Brent <bjc005@shsu.edu>
2020-06-04 16:33:11 -0500

    Draft 1

--- FindCards.py
+++ FindCards.py
@@ -2,12 +2,15 @@ import discord
 from discord.ext import commands
 
 	
-class FindCards:
+class FindCards(commands.Cog):
     """VF Cog that does a Scryfall search"""
 
     def __init__(self, bot):
         self.bot = bot
 
+	@commands.Cog.listener()
+	aync def on_ready():
+        await self.bot.say("FindCards Cog Ready!")
 	
     @commands.command()
     async def mycom(self):
@@ -15,6 +18,49 @@ class FindCards:
 
         await self.bot.say("Yay!")	
 		
+	@commands.command(pass_context=True)
+    async def on_message(self, message):
+        """Build a Scryfall Query"""
+		text = message
+			
+		if text.find("with text ") > 0 :
+			(colortext,withtext) text.split("with text ",1)
+		else:
+			colortext = text
+			withtext = ""
+		
+		
+		if colortext.find("or") > 0 :
+			colors = "color%3E%3D"
+		elsif colortext.find("only") > 0 :
+			colors = "color%3C%3D"
+		else:
+			colors = "color%3D"
+		
+		if colortext.find("red") > 0 : 
+			colors = colors + 'R'			
+		
+		if colortext.find("black") > 0 :
+			colors = colors + 'B'	
+			
+		if colortext.find("blue") > 0 :
+			colors = colors + 'U'	
+			
+		if colortext.find("white") > 0 :
+			colors = colors + 'W'		
+			
+		if colortext.find("green") > 0 :
+			colors = colors + 'G'	
+			
+		theURL = "https://scryfall.com/search?as=grid&order=name&q=legal%3Amodern"
+		
+		if len(colors) > 6:
+			theURL + '+' + colors			
+		
+		if len(withtext) > 0:
+			theURL + '+%28oracle%3A' + withtext.replace(' ','+oracle%3A') + '%29'
+		
+		self.bot.say(theURL)		
 
 def setup(bot):
 		bot.add_cog(FindCards(bot))
